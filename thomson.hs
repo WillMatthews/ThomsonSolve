6fdea390fdff48c2609584f1c2d57293d8a998ed
WillMatthews <will.a.matthews@me.com>
2019-05-08 16:00:11 +0100

    general structure works
    Former-commit-id: 06fb31549adae7112d7ae729d69d92a3a445c63c

--- thomson.hs
+++ thomson.hs
@@ -1,16 +1,20 @@
 import System.Random
 import Data.List
-
+import Data.List.Grouping
 
 -------------- debug constell in 2D ------------------
 
-debugAllPoint :: Float -> Float -> [[Float]]
-debugAllPoint maxVal sep
-    | maxVal <= 0  = error "Negative or zero max value not allowed"
-    | sep    <= 0  = error "Negative or zero separation not allowed"
-    | otherwise   = [ [a,b] | a <- [0.0,sep..maxVal], b <- [0.0,sep..maxVal]]
+debugAllPoint :: Int -> Int -> [[Float]]
+debugAllPoint d k
+    | d < 0     = error "Negative dimensions are not allowed"
+    | k < 0     = error "Negative points is not allowed"
+    | otherwise = splitEvery d ( take (d*k) (randomRs (-0.5,0.5) (mkStdGen 42))  ) 
 
+debugTestStruct :: Float -> Int -> Int -> Int -> ((Float, Int),[([Float],[Float])])
+debugTestStruct dt s d k = ((dt,s), zip (debugAllPoint d k)  ([makeZeroVelocity 2 | cnt <- (debugAllPoint d k)]) )
 
+debugPointStruct :: Int -> Int -> [([Float],[Float])]
+debugPointStruct d k = zip  (debugAllPoint d k) ([makeZeroVelocity 2 | cnt <- (debugAllPoint d k)]) 
 ---------------- vector operations -------------------
 
 -- vec subtraction:  point - loc
@@ -30,10 +34,10 @@ normaliseVec :: [Float] -> [Float]
 normaliseVec  vec = map(/ (l2 vec)) vec
 
 -- normalise if L2 norm of vec is greater than 1
-normIfGtr :: [Float] -> [Float]
-normIfGtr vecToTest
-    | l2(vecToTest) > 1 = normaliseVec vecToTest
-    | otherwise         = vecToTest
+normIfGtr :: [Float] -> Float -> [Float]
+normIfGtr vecToTest testFloat
+    | l2(vecToTest) > testFloat = map (testFloat *) (normaliseVec vecToTest)
+    | otherwise                 = vecToTest
 
 multListOfListByConst :: Float -> [[Float]] -> [[Float]]
 multListOfListByConst multFact listIn = [ map (multFact *) subList | subList <- listIn ] 
@@ -61,9 +65,15 @@ makeAllPointTups k d = sequence [makePointTup d | cnt <- [1..k]]
 showRandomPoints :: Int -> Int -> IO ()
 showRandomPoints a b =  makeAllPointTups a b >>= print
 
+--             dims   points   dt   s
+makeState :: Int -> Int -> Float -> Int -> IO ((Float, Int),[([Float],[Float])])
+makeState d k dt s = (makeAllPointTups k d) >>= \x -> return ((dt,s),x)
+
 --------------- transition function -------------------
 
 -- TODO make a force float a Maybe Float - can be infinity?
+
+-- REMINDER the structure is ((dt, s),[([point],[velocity])]) 
 
 -- for a single PAIR
 getSinglePairForce :: [Float] -> [Float] -> Int -> [Float]
@@ -93,21 +103,35 @@ extractPoints inTupList = [fst(subTup) | subTup <- inTupList]
 extractVelocities :: [([Float],[Float])] -> [[Float]]
 extractVelocities inTupList = [snd(subTup) | subTup <- inTupList]
 
+extractPointStruct :: ((Float, Int),[([Float],[Float])]) -> [([Float],[Float])]
+extractPointStruct globalStruct = snd globalStruct
+
+-- REMINDER the structure is ((dt, s),[([point],[velocity])]) 
+
 
 -- update all velocities THEN positions - STANDARD EULER METHOD
 -- v = v_old + a * dt
--- x = x_old + v * dt
-
 getNewVelocities :: [([Float],[Float])] -> Float -> Int -> [[Float]]
-getNewVelocities pointStruct dt s = [ zipWith(+) (fst into) (snd into) | into <- (zip (extractVelocities pointStruct) ( multListOfListByConst dt (getAccelerations (extractPoints pointStruct) s)))]
+getNewVelocities pointStruct dt s = [ normIfGtr (zipWith(+) (fst into) (snd into)) 1.0 | into <- (zip (extractVelocities pointStruct) ( multListOfListByConst dt (getAccelerations (extractPoints pointStruct) s)))]
 
+-- x = x_old + v * dt
 updateAllPoints :: [([Float],[Float])] -> Float -> Int -> [([Float],[Float])]
-updateAllPoints pointStruct dt s = [  ( (zipWith(+) (fst inTup) (map (dt *) (snd inTup))) , (snd inTup) )  | inTup <- (zip (extractPoints pointStruct) (getNewVelocities pointStruct dt s))]
+updateAllPoints pointStruct dt s = [  ((normIfGtr ( (zipWith(+) (fst inTup) (map (dt *) (snd inTup)))) 1.0) , (snd inTup) )  | inTup <- (zip (extractPoints pointStruct) (getNewVelocities pointStruct dt s))]
+
+--                  dt     s       pointstruct
+transFunction :: ((Float, Int),[([Float],[Float])]) -> ((Float, Int),[([Float],[Float])])
+transFunction globalStruct = ((fst globalStruct), updateAllPoints (snd globalStruct) (fst (fst globalStruct)) (snd (fst globalStruct)))
 
 
+-- Apply Random Start to Trans Function
+--              d      k       dt      s      n
+runIterator :: Int -> Int -> Float -> Int -> Int -> IO ((Float, Int),[([Float],[Float])])
+runIterator d k dt s numIter = makeState d k dt s >>= \x -> return (((take (numIter+1) (iterate transFunction x)) ) !! numIter)
 
 
--- TODO apply transition function over and over
+-- debug code
+--showTest :: Int -> Int -> Float -> Int -> Int -> IO ()
+--showTest d k dt s numIter = (runIterator d k dt s numIter) !! numIter >>= print 
 
 
 ---------------- display functions --------------------
